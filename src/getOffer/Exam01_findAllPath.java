73cbcb17f1850278f218e509f954c86b2717ccaa
Richard <289799074@qq.com>
2018-11-04 22:14:42 +0800

    getOffer

---
+++ src/getOffer/Exam01_findAllPath.java
@@ -0,0 +1,39 @@
+package getOffer;
+
+import java.util.ArrayList;
+
+/**
+ * @Author yc
+ * @Date 2018/11/4 22:00
+ *
+ * 输入一颗二叉树的跟节点和一个整数，打印出二叉树中结点值的和为输入整数的所有路径。
+ * 路径定义为从树的根结点开始往下一直到叶结点所经过的结点形成一条路径。(注意: 在返回值的list中，数组长度大的数组靠前)
+ */
+public class Exam01_findAllPath {
+    public class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+
+        public TreeNode(int val) {
+            this.val = val;
+
+        }
+
+    }
+
+    private ArrayList<ArrayList<Integer>> listAll =  new ArrayList<ArrayList<Integer>>();
+    private ArrayList<Integer> list = new ArrayList<Integer>();
+    public ArrayList<ArrayList<Integer>> FindPath(TreeNode root,int target) {
+        if(root == null) return listAll;
+        list.add(root.val);
+        target -= root.val;
+        if(target == 0 && root.left == null && root.right == null)
+            listAll.add(new ArrayList<Integer>(list));
+        FindPath(root.left, target);
+        FindPath(root.right, target);
+        list.remove(list.size()-1);
+        return listAll;
+    }
+
+}
