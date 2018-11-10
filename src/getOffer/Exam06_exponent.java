4db23bccd9a4ca6c37b5d8f6dd2158639e8969e3
Richard <289799074@qq.com>
2018-11-10 21:24:26 +0800

    getOffer

---
+++ src/getOffer/Exam06_exponent.java
@@ -0,0 +1,13 @@
+package getOffer;
+
+/**
+ * @Author yc
+ * @Date 2018/11/10 21:02
+ * 给定一个double类型的浮点数base和int类型的整数exponent。求base的exponent次方。
+ */
+public class Exam06_exponent {
+    public double Power(double base, int exponent) {
+        return Math.pow(base, exponent);
+    }
+
+}
